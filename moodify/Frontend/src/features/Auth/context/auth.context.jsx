1ddd1a47e8a7f8cf18e8306fffd9bc3f18c997b0
parvdube-ux <parvdube@gmail.com>
2026-06-26 09:30:05 +0530

    provider fixed

--- moodify/Frontend/src/features/Auth/context/auth.context.jsx
+++ moodify/Frontend/src/features/Auth/context/auth.context.jsx
@@ -29,7 +29,7 @@ export const AuthProvider = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user, setuser, loading }}>
+    <AuthContext.Provider value={{ user, setuser, loading, setloading }}>
       {children}
     </AuthContext.Provider>
   );
